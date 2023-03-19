2cbed0e52b47f312b11a8956497feb245770783b
courageous-coward <battlecast2491@gmail.com>
2023-03-19 15:04:53 +0900

    2023-03-19 pm 3:04

---
+++ ex1_2/ex1_2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "a\n";
+    std::cout << "b\n";
+    std::cout << "c";
+    return 0;
+}
---
+++ ex1_3/ex1_3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << 1 << "a" << 10 << "b" << 55 << "c";
+    return 0;
+}
