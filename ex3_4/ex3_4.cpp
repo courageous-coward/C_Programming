19849c1f446507b7b3d3d3ff03787ca537e85463
courageous-coward <battlecast2491@gmail.com>
2023-04-07 15:50:30 +0900

    2023-04-07 pm 3:50

---
+++ ex3_4/ex3_4.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class Circle
+{
+public:
+    Circle();
+    Circle(int r);
+    int radius;
+    double getArea();
+};
+
+Circle::Circle():Circle(1)
+{
+    
+}
+
+Circle::Circle(int r)
+{
+    radius = r;
+    cout << "radius is " << radius << " circle of generation " << endl;
+}
+
+double Circle::getArea()
+{
+    return 3.14 * radius * radius;
+}
+
+int main()
+{
+    Circle donut;
+    double area = donut.getArea();
+    cout << "area of donut is " << area << endl;
+
+    Circle pizza(30);
+    area = pizza.getArea();
+    cout << "area of pizza is " << area << endl;
+}
