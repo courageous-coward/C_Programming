9ab38a21d4d8adfbcf0447cff38a354df2344b04
courageous-coward <battlecast2491@gmail.com>
2023-04-07 14:52:02 +0900

    2023-04-07 pm 2:51

---
+++ ex3_2/ex3_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+public:
+    int width;
+    int height;
+    int getArea();
+};
+
+int Rectangle::getArea()
+{
+    return width * height;
+}
+
+
+int main()
+{
+    Rectangle rect;
+    rect.width = 3;
+    rect.height = 5;
+    cout << "area of rectangle is " << rect.getArea() << endl;
+}
